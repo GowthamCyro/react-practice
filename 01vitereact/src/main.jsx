04a4b46a7e27a76c85f5c095eabe352a64215e30
GowthamCyro <cyrogamer.yt@gmail.com>
2024-08-13 17:00:00 +0530

    comments updated

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -9,3 +9,6 @@ createRoot(document.getElementById('root')).render(
       <App />
     </>
 )
+
+// createRoot runs the algorithm of the latest react fibre and uses the concept reconciliation 
+// If we generaly pass the javascript without react we need to render by dom by creating using id but incase here render() function takes care of this job
