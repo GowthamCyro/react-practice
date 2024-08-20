b500daed7ba069087305e5df6c963ff487677a31
GowthamCyro <cyrogamer.yt@gmail.com>
2024-08-20 13:09:21 +0530

    Blog Project

---
+++ megaBlog/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState,useEffect } from 'react'
+import {useDispatch} from "react-redux"
+import './App.css'
+import authService from "./appwrite/auth"
+import {login,logout} from "./store/authSlice"
+import {Footer,Header} from "./components/index"
+import {Outlet} from "react-router-dom"
+
+function App() {
+
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+  
+
+  if(loading){
+    return(
+      <>
+      Loading....
+    </>
+    )
+    
+  }
+  else{
+    return(
+      <div className='min-h-screen flex flex-wrap content-between bg-cyan-500'>
+        <div className='w-full block'>
+          <Header />
+          <main>
+            TODO : <Outlet />
+          </main>
+          <Footer />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
